d0f04c0f1c915a58cbe17cbf4d43b73684b37f10
guillermogut <77817539+guillermogut@users.noreply.github.com>
2021-09-15 23:26:16 -0400

    Add files via upload

---
+++ hw2.m
@@ -0,0 +1,25 @@
+%%
+addpath('..\matlab_utils')
+syms th1 th2 th3 F1 F2 t1 t2 
+% 
+
+%th1,2 and 3 are angles
+%length of arms
+l1 = 1;
+l2 = 1;
+l3 = 1;
+
+J = jacobian([l1*cos(th1) + l2* cos(th1 + th2) + l3* cos(th1+th2+th3); l1*sin(th1) + l2* sin(th1 + th2) + l3* sin(th1+th2+th3)],[th1;th2;th3]);
+
+
+%relation of torque to forces using Jacobian
+
+%torque * dq - F * dx = 0
+
+%torque * dq = F * dx
+
+%torque = F * (dx/dq)
+
+%torque = J^T * F
+
+
